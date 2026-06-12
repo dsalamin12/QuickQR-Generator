216de7b2ed89971e1143e4e404b50f05608c8e71
dsalamin12 <dsalamin12@gmail.com>
2026-06-12 11:04:37 +0600

    Create footer.js
    Adding Footer

---
+++ assets/footer.js
@@ -0,0 +1,24 @@
+document.getElementById("footer-placeholder").innerHTML = `
+<footer style="
+padding:25px;
+text-align:center;
+border-top:1px solid var(--border);
+margin-top:50px;
+">
+
+<p>
+© 2026 QuickQR
+</p>
+
+<p style="margin-top:10px;">
+
+<a href="privacy.html">Privacy</a>
+|
+<a href="about.html">About</a>
+|
+<a href="contact.html">Contact</a>
+
+</p>
+
+</footer>
+`;
